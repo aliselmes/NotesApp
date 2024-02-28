6d229d238b8a39cc4f748ecdb8e13c89ca5e4013
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-02-28 12:34:35 -0600

    First Code Commit

---
+++ NotesApp/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
+
+namespace NotesApp.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string? Title { get; set; }
+
+        public string? Description { get; set; }
+
+        [Display(Name = "Date Created")]
+        [DataType(DataType.DateTime)]
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+    }
+}
