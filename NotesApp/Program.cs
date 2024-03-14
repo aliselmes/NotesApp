7fcb433581f17049f1b6fb50d423c5fb0781b9dd
Alasdair Selmes <aselmes@sullivanbranding.com>
2024-03-14 16:59:46 -0500

    UI changes

--- NotesApp/Program.cs
+++ NotesApp/Program.cs
@@ -100,7 +100,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Notes}/{action=Index}/{id?}");
 app.MapRazorPages();
 
 app.Run();
